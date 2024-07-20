9b8cc6f0288921cc452d2c4c630f8e77ef457c95
Win10 <Win10@DESKTOP-VUC6R45>
2024-07-20 09:59:29 +0530

    Class pushing

---
+++ SUBBU_MAVEN/src/test/java/subu/maven/Location.java
@@ -0,0 +1,14 @@
+package subu.maven;
+
+public class Location {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("location:-puri");	
+		
+		
+		
+	}
+
+}
